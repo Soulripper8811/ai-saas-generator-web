fdb7ff86413fe2dc4a7d36a5588831708fce42cb
Soulrippper8811 <134708634+Soulripper8811@users.noreply.github.com>
2025-02-26 22:51:45 +0530

    added effect

--- components/LandingHero.tsx
+++ components/LandingHero.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import Link from "next/link";
-import TypewriterComponent from "typewriter-effect";
 import { Button } from "./ui/button";
 import { useAuth } from "@clerk/nextjs";
+import { TypeAnimation } from "react-type-animation";
 
 const LandingHero = () => {
   const { isSignedIn } = useAuth();
@@ -12,18 +12,22 @@ const LandingHero = () => {
       <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
         <h1>The Best AI tool for</h1>
         <div className=" text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-          <TypewriterComponent
-            options={{
-              strings: [
-                "Chatbot.",
-                "Photo Generation.",
-                "Code Generation.",
-                "Music Generation.",
-                "Video Generation.",
-              ],
-              autoStart: true,
-              loop: true,
-            }}
+          <TypeAnimation
+            sequence={[
+              // Same substring at the start will only be typed out once, initially
+              "Chatbot.",
+              3 * 1000, // wait 1s before replacing "Mice" with "Hamsters"
+              "Photo Generation.",
+              3 * 1000,
+              "Code Generation.",
+              3 * 1000,
+              "Music Generation.",
+              3 * 1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ display: "inline-block" }}
+            repeat={Infinity}
           />
         </div>
       </div>
